1485eb7d89e0ea446c4af7c657009ce7b9960e8f
Julian Doherty (madlep) <madlep@madlep.com>
2019-03-20 16:10:10 +1100

    install distillery

--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule Click.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:distillery, "~> 1.5.5"}
     ]
   end
 
