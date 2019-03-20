105b40acd49563a39eb9ab65b8248b02544ad7c4
Julian Doherty (madlep) <madlep@madlep.com>
2019-03-20 16:50:04 +1100

    use click.madlep.com

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ config :click, ClickWeb.Endpoint,
   http: [port: {:system, "PORT"}], # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
   server: true, # Without this line, your app will not start the web server!
   secret_key_base: "${SECRET_KEY_BASE}",
-  url: [host: "${APP_NAME}.gigalixirapp.com", port: 443],
+  url: [host: "click.madlep.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 config :click, Click.Repo,
